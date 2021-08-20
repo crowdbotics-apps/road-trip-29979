a420b85be8cc07495570cbf5ca39444460b81885
Crowdbotics <team@crowdbotics.com>
2021-08-20 17:14:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "road_trip_29979Identifier",
+  url: "https://road_trip_29979Identifier.botics.co",
+  api: "https://road_trip_29979Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
